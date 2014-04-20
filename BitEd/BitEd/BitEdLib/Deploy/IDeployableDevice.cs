fcaa45972a21774269afd36ec4ba6fc9d84e6f08
Trym Nilsen <trym.nilsen@gmail.com>
2014-04-20 14:00:28 +0200

    Project Cleanup

--- BitEd/BitEd/BitEdLib/Deploy/IDeployableDevice.cs
+++ BitEd/BitEd/BitEdLib/Deploy/IDeployableDevice.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BitEdToolRender.Scene
+namespace BitEdLib.Deploy
 {
-    class Light
+    public interface IDeployableDevice
     {
     }
 }
--- BitEd/BitEd/BitEdLib/IDeployableDevice.cs
+++ BitEd/BitEd/BitEdLib/IDeployableDevice.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BitBuild
 {
-    public class Class1
+    public interface IDeployableDevice
     {
     }
 }
