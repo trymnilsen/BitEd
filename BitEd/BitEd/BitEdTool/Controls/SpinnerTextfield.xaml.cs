b0eea6cd9b433271da1b03f735dcda5e7aecbe47
Trym Nilsen <trym.nilsen@gmail.com>
2014-05-08 21:47:48 +0200

    ValueSpinners

---
+++ BitEd/BitEd/BitEdTool/Controls/SpinnerTextfield.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BitEdTool.Controls
+{
+    /// <summary>
+    /// Interaction logic for SpinnerTextfield.xaml
+    /// </summary>
+    public partial class SpinnerTextfield : UserControl
+    {
+
+    public static readonly DependencyProperty NumValueProperty = DependencyProperty.Register("NumValue", typeof(int), typeof(SpinnerTextfield));
+
+    public int NumValue
+    {
+        get { return (int)GetValue(NumValueProperty); }
+        set
+        {
+            SetValue(NumValueProperty, value);
+            txtNum.Text = value.ToString();
+        }
+    }
+
+    public SpinnerTextfield()
+    {
+        InitializeComponent();
+        NumValue = 0;
+        txtNum.Text = "0";
+    }
+
+    private void cmdUp_Click(object sender, RoutedEventArgs e)
+    {
+        NumValue++;
+    }
+
+    private void cmdDown_Click(object sender, RoutedEventArgs e)
+    {
+        NumValue--;
+    }
+
+    private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        int parsedNum = 0;
+        if (!int.TryParse(txtNum.Text, out parsedNum))
+            txtNum.Text = NumValue.ToString();
+    }
+    }
+}
---
+++ BitEd/BitEd/BitEdTool/Converters/SubtractConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BitEdTool.Converters
+{
+    /// <summary>
+    /// Subtracts the param from the binding
+    /// </summary>
+    public class SubtractConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is Double && parameter is String)
+            {
+                Double currentValue = System.Convert.ToDouble(value);
+                //Paramter is treated as a string, parse it
+                Double subtraction = Double.Parse(parameter as String);
+                return currentValue - subtraction;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
