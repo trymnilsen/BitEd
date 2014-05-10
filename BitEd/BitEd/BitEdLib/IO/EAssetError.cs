e5c62abc4bb058365ad74154fa481270574adb32
Trym Nilsen <trym.nilsen@gmail.com>
2014-05-11 00:41:12 +0200

    Loading of sprites

---
+++ BitEd/BitEd/BitEdLib/IO/EAssetError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitEdLib.IO
+{
+    public enum EAssetError
+    {
+        None,
+        IOError,
+        IOAccessError,
+        IOFileNotFound
+    }
+}
--- BitEd/BitEd/BitEdTool/ViewModel/Timeline/TimelineSpriteViewModel.cs
+++ BitEd/BitEd/BitEdTool/ViewModel/Timeline/TimelineSpriteViewModel.cs
@@ -1,19 +1,57 @@
-﻿using BitEdLib.Model.Assets.Sprite;
+﻿using BitEdLib.IO;
+using BitEdLib.Model.Assets.Sprite;
+using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace BitEdTool.ViewModel.Timeline
 {
-    public class TimelineSpriteViewModel
+    public class TimelineSpriteViewModel:ViewModelBase
     {
+        private BitmapImage assetSource;
+        private string filePath;
+        //properties
         public SpriteFrame Model { get; set; }
-        public string FilePath { get; set; }
+        public string FilePath
+        {
+            get { return filePath; }
+            set
+            {
+                if(filePath != value)
+                {
+                    filePath = value;
+                    RaisePropertyChanged("FilePath");
+                }
+            }
+        }
+        public BitmapImage AssetSource
+        {
+            get { return assetSource; }
+            set
+            {
+                if (assetSource != value)
+                {
+                    assetSource = value;
+                    RaisePropertyChanged("AssetSource");
+                }
+            }
+        }
+        public EAssetError ErrorType { get; set; }
         public TimelineSpriteViewModel(SpriteFrame model)
         {
             this.Model = model;
+        }
+        private void LoadAsset()
+        {
+            using(FileStream fs = new FileStream(FilePath,FileMode.Open))
+            {
+
+            }
         }
     }
 }
