85fd98146c5194756e39a8c006aa3d60325f0e67
Trym Nilsen <niltry10@nith.no>
2014-05-14 15:57:15 +0200

    message

---
+++ BitEd/BitEd/BitEdTool/Messages/Assets/AssetNameChangedMessage.cs
@@ -0,0 +1,24 @@
+﻿using BitEdTool.ViewModel.Asset;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitEdTool.Messages.Assets
+{
+    internal class AssetNameChangedMessage
+    {
+        public AssetListEntryViewModel Item { get; set; }
+
+        private AssetNameChangedMessage(AssetListEntryViewModel item)
+        {
+            this.Item = item;
+        }
+        public static void Send(AssetListEntryViewModel newSelectedItem)
+        {
+            Messenger.Default.Send<AssetNameChangedMessage>(new AssetNameChangedMessage(newSelectedItem));
+        }
+    }
+}
